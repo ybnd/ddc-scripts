5ba759e4747a113b4315fa38e87ad46d945fd09c
ybnd <ybnd@tuta.io>
2023-04-04 22:11:03 +0200

    Get current value before setting

--- common.sh
+++ common.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-CONFIG_PATH=~/.config/ddc-scripts
+CONFIG_DIR=$HOME/.config/ddc-scripts
+CONFIG_PATH=$CONFIG_DIR/config.json
+DISPLAYS=$CONFIG_DIR/displays
+JOBS=$CONFIG_DIR/pids
 ABOUT='configuration file for https:/github.com/ybnd/ddc-scripts'
 
+debug() {
+    if [[ -n $DDC_SCRIPTS_DEBUG ]]; then
+        echo "ddc-scripts -- $1"
+    fi
+}
 
-config_edit() {     
+
+config_edit() {
+    debug "Writing to config: $@"
     CONFIG=$(jq "$@" "$CONFIG_PATH")
     echo "$CONFIG" > $CONFIG_PATH
 }
@@ -22,26 +32,29 @@ config_read_optional() {
     fi
 }
 
-displays() {
-    config_read '.displays | keys[]'
-}
-
-aliases() {
-    config_read '.aliases | keys[]'
-}
-
 dealias() {
     VALUE_IN="$1"
     if [[ -z "$VALUE_IN" ]]; then
-	echo ""
-    elif [[ $(aliases) == *"$VALUE_IN"* ]]; then
-        echo $(config_read ".aliases.\"$VALUE_IN\"")
+	    echo ""
     else
-        echo "$VALUE_IN"
+        DEALIASED=$(config_read ".aliases.\"$VALUE_IN\" | values")
+        if [[ -n $DEALIASED ]]; then
+            echo "$DEALIASED"
+        else
+            echo "$VALUE_IN"
+        fi
     fi
 }
 
+display_code() {
+    config_read ".displays[] | select(.name == \"$1\") | .code" || echo $1
+}
+
 init() {
+    debug "Initializing"
+    mkdir -p $CONFIG_DIR
+
+    # Clear previous configuration
     echo "{}" > "$CONFIG_PATH"
     
     config_edit ".about = \"$ABOUT\""
@@ -56,42 +69,61 @@ detect_displays() {
     if [[ 0 -ne "$(config_read '.displays | length')" ]]; then 
         config_edit ".displays = {}"
     fi
+    rm $DISPLAYS
 
     # We use i2c buses instead of ddcutil's display IDs since those don't work properly when called in parallel
     ddcutil detect | sed -rn 's/.*\/dev\/i2c-([[:digit:]])/\1/p' | while read DISPLAY; do
         config_edit ".displays.\"$DISPLAY\" |= {}"
+        config_edit ".displays.\"$DISPLAY\".code |= \"$DISPLAY\""
         config_edit ".displays.\"$DISPLAY\".name |= \"Display $DISPLAY\""
+        echo $DISPLAY >> $DISPLAYS
     done
 }
 
 ddc_adjust() {
+    debug "ddc_adjust $@"
     CONTROL="$1"
-    VCP=$(config_read ".vcp.\"$CONTROL\"")
-    TO="$2"
+    TO=$2
 
-    displays | while read DISPLAY ; do
-        NAME=$(config_read ".displays.\"$DISPLAY\".name")
-        FROM=$(config_read ".displays.\"$DISPLAY\".\"$CONTROL\"")
-        
-	if [[ -z "$TO" ]]; then
-	    echo "$NAME: $CONTROL $FROM"
-	elif [[ $FROM -ne $TO ]]; then
-            echo "$NAME: $CONTROL $FROM -> $TO"
-            # don't wait for ddcutil to finish so the adjustment happens more or less in parallel
-            ddcutil -b "$DISPLAY" setvcp "$VCP" "$TO" &
-            
-            config_edit ".displays.\"$DISPLAY\".\"$CONTROL\" |= \"$TO\""
-        else
-            echo "$NAME: $CONTROL $TO"
-        fi
-    done
+    while read DISPLAY ; do
+        _ddc_adjust $DISPLAY $CONTROL $TO &
+    done < $DISPLAYS
+
+    jobs -p > $JOBS
+    while read JOB ; do 
+        wait $JOB || echo "Failed!"
+    done < $JOBS
+
+    rm $JOBS
+}
+
+_ddc_adjust() {
+    debug "_ddc_adjust $@"
+
+    DISPLAY="$1"
+    CONTROL="$2"
+    TO="$3"
+
+    VCP=$(config_read ".vcp.\"$CONTROL\"")
+    NAME=$(config_read ".displays.\"$DISPLAY\".name")
+    # todo: messy sed
+    FROM=$(ddcutil -b "$DISPLAY" getvcp "$VCP" | sed -r 's/[^=]+= *([^=]+)[\),].*/\1/')
+
+    if [[ -z "$TO" ]]; then
+        echo "$NAME: $CONTROL $FROM"
+    elif [[ $FROM -ne $TO ]]; then
+        echo "$NAME: $CONTROL $FROM -> $TO"
+        ddcutil -b "$DISPLAY" setvcp "$VCP" "$TO"
+    else
+        echo "$NAME: $CONTROL $TO"
+    fi
 }
 
 
-if [[ ! -f "$CONFIG_PATH" || -z $(cat "$CONFIG_PATH") ]]; then
+if [[ ! -d "$CONFIG_DIR" || ! -f "$CONFIG_PATH" || -z $(cat "$CONFIG_PATH") ]]; then
     init
 fi
 
-if [[ 0 -eq "$(config_read '.displays | length')" ]]; then 
+if [[ ! -f "$DISPLAYS" || -z $(cat "$DISPLAYS") ]]; then 
     detect_displays
 fi
